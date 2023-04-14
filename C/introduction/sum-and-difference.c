b3f7f58f784d47b066154f5513ad815f11fb39b3
irviner26 <130742004+irviner26@users.noreply.github.com>
2023-04-14 16:24:29 +0700

    Create sum-and-difference.c

---
+++ C/introduction/sum-and-difference.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int number1;
+    int number2;
+    float number3;
+    float number4;
+    
+    scanf("%d %d", &number1, &number2);
+    scanf("%f %f", &number3, &number4);
+    
+    printf("%d %d\n", number1+number2, number1-number2);
+    printf("%0.1f %0.1f", number3+number4, number3-number4);
+    return 0;
+}
